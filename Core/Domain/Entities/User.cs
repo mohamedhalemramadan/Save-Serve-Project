4f88c6a7b25f8f4fb54c925f6e230129316adc25
mohamed Abdel halim <mahamedhalemramadan@gmail.com>
2026-04-08 12:30:59 +0200

    ffff

---
+++ Core/Domain/Entities/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace Domain.Entities
+{
+    
+    public class User : IdentityUser
+    {
+        public string DisplayName { get; set; }
+        public Address Address { get; set; }
+
+        public string Role { get; set; } // Restaurant/Consumer/Charity/Admin
+        public string Phone { get; set; }
+        public string Status { get; set; } // Active/Inactive
+        public string AddressText { get; set; }
+
+        // Navigation Properties
+
+
+        public Restaurant Restaurant { get; set; }
+        public Charity Charity { get; set; }
+        public Consumer Consumer { get; set; }
+        public DeliveryPartner DeliveryPartner { get; set; }
+
+
+    }
+}
